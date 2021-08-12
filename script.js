3ccdbe5537bb34f08f0973c3cb109f73b38c77ea
Urxpaly45 <kalen.shamy@gmail.com>
2021-08-11 20:54:47 -0400

    Fix usage of class & id

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /* Open when someone clicks on the span element */
 function openNav() {
-  document.getElementById("mobNavOverlay").style.width = "100%";
+  document.getElementById("mobileNavOverlay").style.width = "100%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-  document.getElementById("mobNavOverlay").style.width = "0%";
+  document.getElementById("mobileNavOverlay").style.width = "0%";
 }
