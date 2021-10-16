1e518dd1e2a1ac9cec07697f7168ebc580d8f4b0
Urxpaly45 <kalen.shamy@gmail.com>
2021-10-16 18:08:11 -0400

    Credit in comment

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-/* Open when someone clicks on the span element */
+
+/* Made by Kalen Shamy */
+
 function openNav() {
   document.getElementById("mobileNavOverlay").style.width = "100%";
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
   document.getElementById("mobileNavOverlay").style.width = "0%";
 }
