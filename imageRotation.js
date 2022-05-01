be0659153c1c0a183d2eb91b3fbcc64118ce50f9
Urxpaly45 <kalen.shamy@gmail.com>
2022-04-30 22:20:56 -0400

    index page: image rotation, map change (tablet & desktop)

---
+++ imageRotation.js
@@ -0,0 +1,41 @@
+const imageCount = 7;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var currentImg = 1;
+
+async function imageChange(num) {
+  if (num == currentImg) return;
+  document.getElementById("WB" + currentImg).style.backgroundColor = "";
+  document.getElementById("WB" + num).style.backgroundColor = "rgb(175,175,175)";
+
+  document.getElementById("WI" + currentImg).style.opacity = 0;
+  document.getElementById("WI" + num).style.opacity = 0;
+  await sleep(500);
+  document.getElementById("WI" + currentImg).style.display = "none";
+  document.getElementById("WI" + currentImg).style.opacity = 1;
+
+  document.getElementById("WI" + num).style.display = "block";
+  await sleep(250);
+  document.getElementById("WI" + num).style.opacity = 1;
+  currentImg = num;
+}
+
+function lastImage() {
+  if (currentImg == 1) imageChange(imageCount);
+  else imageChange(currentImg-1);
+}
+
+function nextImage() {
+  if (currentImg == imageCount) imageChange(1);
+  else imageChange(currentImg+1);
+}
+
+function startRotation() {
+  document.getElementById("WB1").style.backgroundColor = "rgb(175,175,175)";
+  setInterval(nextImage, 5 * 1000);
+}
+
+window.onload = startRotation;
