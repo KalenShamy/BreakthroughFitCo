193f58818f902c26be761b19d892418e16f6bc3c
jallyne <jake@breakthroughfitco.com>
2022-06-11 13:43:58 -0400

    add substitutes for incomplete profiles

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ if (window.location.pathname.indexOf("team") != -1) {
     let memberDiv = "";
     memberDiv += "<!-- " + staff[i]["name"] + "-->\n";
     memberDiv += "<div class=\"teamMember\">\n";
-    memberDiv += "<img loading=\"lazy\" alt=\"\" src=\"Pictures/" + staff[i]["img"] + "\">\n";
+    memberDiv += "<img loading=\"lazy\" alt=\"\" src=\"Pictures/Staff/" + staff[i]["img"] + "\">\n";
     memberDiv += "<div class=\"teamMemberText\">\n";
     memberDiv += "<h1 class=\"teamMemberName\">" + staff[i]["name"] + " - " + staff[i]["title"] + "</h1>\n";
-    memberDiv += "<p>" + staff[i]["bio"] + "</p>\n";
+    memberDiv += "<p>" + (staff[i]["bio"] != "" && staff[i]["bio"] || "Bio in progress!") + "</p>\n";
     if (staff[i]["certifications"] != undefined) {
       memberDiv += "<h1 class=\"teamMemberName\">Certifications</h1>\n";
       let certs = "";
@@ -36,7 +36,7 @@ if (window.location.pathname.indexOf("team") != -1) {
       }
       memberDiv += "<p>" + certs + "</p>\n";
     }
-    memberDiv += "<a class=\"greyButton\" target=\"_blank\" href=\"mailto:" + staff[i]["email"] + "?subject=Breakthrough%20Fit%20Co\">Contact</a>\n";
+    memberDiv += "<a class=\"greyButton\" target=\"_blank\" href=\"mailto:" + staff[i]["email"] + "?subject=Breakthrough%20Fit%20Co" + (staff[i]["email"].split("@")[0] == "jake" && staff[i]["name"] != "Jake Allyne" && ": " + staff[i]["name"] || "") + "\">Contact</a>\n";
     memberDiv += "</div>\n";
     memberDiv += "</div>\n";
     memberDivs += memberDiv;
