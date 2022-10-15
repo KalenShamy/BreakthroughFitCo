c66f1744453c2990abb8cbf7e26efa028e1aea45
kalenshamy <kalen.shamy@gmail.com>
2022-10-15 15:27:06 -0400

    translate to sveltekit & ts

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -10,7 +10,35 @@
         <div class="serviceTop">
             <img loading="lazy" alt="" src="Pictures/personalTraining.webp">
             <h4>Personal Training</h4>
-            <p>First session on us!<br><br><b>Single Session Rates:</b><br>60 mins: $120/session<br>30 mins: $75/session<br><br><b>Packages:</b><br><u>&nbsp;60 mins&nbsp;</u><br>1x Per Week: $108/session<br>2x Per Week: $99/session<br>3x Per Week: $95/session<br><br><u>&nbsp;30 mins&nbsp;</u><br>1x Per Week: $70/session<br>2x Per Week: $65/session<br>3x Per Session: $60/session</p>
+            <p>First session on us!
+                <br>
+                <br>
+                <b>Single Session Rates:</b>
+                <br>
+                60 mins: $120/session
+                <br>
+                30 mins: $75/session
+                <br>
+                <br>
+                <b>Packages:</b>
+                <br>
+                <u>&nbsp;60 mins&nbsp;</u>
+                <br>
+                1x Per Week: $108/session
+                <br>
+                2x Per Week: $99/session
+                <br>
+                3x Per Week: $95/session
+                <br>
+                <br>
+                <u>&nbsp;30 mins&nbsp;</u>
+                <br>
+                1x Per Week: $70/session
+                <br>
+                2x Per Week: $65/session
+                <br>
+                3x Per Session: $60/session
+            </p>
         </div>
         <a class="greyButton" href="mailto:jake@breakthroughfitco.com?subject=Personal%20Training">Learn More</a>
     </div>
@@ -18,7 +46,36 @@
         <div class="serviceTop">
         <img loading="lazy" src="Pictures/IGNITE.webp" alt="" style="width: 75%; padding: 12.5%;">
         <h4>IGNITE</h4>
-        <p>IGNITE offers fun, inclusive, and structured programming to create community and enhance the lives of those we work with.<br><br><b>Packages:</b><br>1x Per Week: $199/month<br>2x Per Week: $249/month<br><br><b>Includes:</b><br>Weekly classes<br>Complimentary Introductory Training Session<br>Community Nights<br><br><span class="asterisks">*Classes will have always 2 qualified coaches and never exceed 5 participants to ensure personalization, progress, and results.<br>**Reimbursement and direct pay options available via self-direction</span></p>
+        <p>
+            IGNITE offers fun, inclusive, and structured programming to create community and enhance the lives of those we work with.
+            <br>
+            <br>
+            <b>Packages:</b>
+            <br>
+            1x Per Week: $199/month
+            <br>
+            2x Per Week: $299/month
+            <br>
+            3x Per Week: $359/month
+            <br>
+            <br>
+            <b>Includes:</b>
+            <br>
+            Weekly classes
+            <br>
+            Complimentary Introductory Training Session
+            <br>
+            Community Nights
+            <br>
+            <br>
+            <span class="asterisks">
+                *Classes will have always 2 qualified coaches and never exceed 5 participants to ensure personalization, progress, and results.
+                <br>
+                **Reimbursement and direct pay options available via self-direction
+                <br>
+                ***Spots filling quickly, <a href="mailto:jake@breakthroughfitco.com?subject=IGNITE">get yours now</a>!!!
+            </span>
+        </p>
         </div>
         <a class="greyButton" href="ignite">Learn More</a>
     </div>
--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,74 +1,96 @@
+<script lang="ts">
+    var staff = [
+        {
+            "name": "Jake Allyne",
+            "title": "President & CEO",
+            "img": "JakeAllyne.webp",
+            "email": "jake@breakthroughfitco.com",
+            "bio": `Jake Allyne is the founder and CEO of Breakthrough Fit Co. With 8 years of experience, Jake specializes in exercise programming to establish proper biomechanics and correct movement patterns within a wide variety of goals. Jake also has a great deal of experience with the special needs community and it is his passion to create equal access to health and fitness for that community. His favorite food is sushi and you can often find him listening to Frank Sinatra. A quote he lives by is "if you can dream it, you can achieve it."`,
+            "certifications": [
+                "NASM CPT",
+                "NASM PES",
+                "NASM CES",
+                "NASM FNS",
+                "AFFA GFI",
+                "AUTISM FITNESS",
+                "USAW STRENGTH COACH",
+                "MADDOG SPINNING",
+                "DAS SPONSORED ATHLETE",
+            ]
+        }, // Jake
+        {
+            "name": "Stephen Goodman",
+            "title": "CFO & COO",
+            "img": "StephenGoodman.webp",
+            "email": "steve@breakthroughfitco.com",
+            "bio": `Stephen is our CFO at Breakthrough Fit Co. He holds his Bachelor's Degree in Kinesiology from SUNY Cortland, as well as his MBA from Mercy College, graduating Summa Cum Laude. Stephen is a life-long resident of Westchester, and in his free time, enjoys exploring the beautiful nature the county has to offer, from hiking Bear Mountain, to the scenic Riverwalk in Tarrytown. Stephen is also a head coach for the Sound Shore Stars Special Olympics Team. His favorite food is Chicken Parmigiana! A quote he lives by is "those who say they can and those that say they can't are both usually right."`,
+        }, // Stephen
+        {
+            "name": "Alexis Grier",
+            "title": "Coach",
+            "img": "AlexisGrier.webp",
+            "email": "jake@breakthroughfitco.com",
+            "bio": `Alexis has been working with the special needs community since 2012. She has a passion for working with children and families, fitness, and filmmaking. Her goal is to allow individuals to explore their physical and creative abilities, building self esteem and confidence in their ability to positively express themselves. She credits working as an Adaptive Special Needs Trainer and Behavioral Therapist as one of the most rewarding experiences in her career to date.`,
+            "certifications": [
+                "SPECIAL STRONG CERTIFIED",
+            ]
+        }, // Alexis
+        {
+            "name": "Vadid Bienaime",
+            "title": "Coach",
+            "img": "VadidBienaime.webp",
+            "email": "jake@breakthroughfitco.com",
+            "bio": `Vadid is from Haiti and has been working as a trainer since 2018. He has prior experience working with the special needs community in their group homes. He has a deep passion for fitness that he'd love to share with everyone, no matter their abilities. He enjoys running in his free time and his favorite food is pizza.`,
+            "certifications": [
+                "SPECIAL STRONG CERTIFIED",
+            ]
+        }, // Vadid
+        {
+            "name": "Taje Champagnie",
+            "title": "Coach",
+            "img": "TajeChampagnie.webp",
+            "email": "taje@breakthroughfitco.com",
+            "bio": `As a professional with 6 years of training experience, Taje has come to the understanding that the greatest limiter is choice. He is a NASM certified Personal Trainer and specializes in balance and stability. Regardless of client goals, balance is the lowest common denominator, so his specialty is broad enough to facilitate a variety of physiological needs. He does this in order to remove as many obstacles as possible for his clients so that they get closer to that great limiter; choice. Once he helps his clients get there, they feel secure enough to make the decisions they need to regain, retain, or expand their health. A quote he lives by is "those who ask for help -earnestly- deserve help."`,
+        }, // Taje
+        {
+            "name": "Michelle Pachon",
+            "title": "Massage Therapist",
+            "img": "MichellePachon.webp",
+            "email": "michellesmassages14@gmail.com",
+            "bio": `Meet Michelle Pachon, our NYS Licensed Massage Therapist and 200-hour Yoga Alliance Certified Instructor! Michelle has been a practicing Massage Therapist since 2015 and has extensive background in different restorative and regenerative massage styles such as Swedish, Myofascial release, Sport, Lymphatic, Prenatal, and Geriatric massage. She’s also known for her amazing Hatha, Power flow, Vinyasa flow and deep stretch yoga classes. Michelle is the best addition to your self-care routine!`,
+        }, // Michelle
+    ]
+</script>
+
 <div id="ourteam">
-    <noscript>
-        Team requires JavaScript - <u>MAY NOT BE UP TO DATE</u>
-    </noscript>
     <div id="teamContents" class="paddingPadder centerBox" style="margin-bottom: 0">
-        <!-- Jake Allyne-->
-        <div class="teamMember">
-            <img loading="lazy" alt="" src="Pictures/Staff/JakeAllyne.webp">
-            <div class="teamMemberText">
-                <h1 class="teamMemberName">Jake Allyne - President &amp; CEO</h1>
-                <p>Jake Allyne is the founder and CEO of Breakthrough Fit Co. With 8 years of experience, Jake specializes in exercise programming to establish proper biomechanics and correct movement patterns within a wide variety of goals. Jake also has a great deal of experience with the special needs community and it is his passion to create equal access to health and fitness for that community. His favorite food is sushi and you can often find him listening to Frank Sinatra. A quote he lives by is:<br><br>"If you can dream it, you can achieve it."</p>
-                <h1 class="teamMemberName">Certifications</h1>
-                <p>NASM CPT - NASM PES - NASM CES - NASM FNS - AFFA GFI - AUTISM FITNESS - USAW STRENGTH COACH - MADDOG SPINNING - DAS SPONSORED ATHLETE</p>
-                <a class="greyButton" target="_blank" href="mailto:jake@breakthroughfitco.com?subject=Breakthrough%20Fit%20Co">Contact</a>
+        {#each staff as member}
+            <div class="teamMember">
+                <img loading="lazy" alt="" src="Pictures/Staff/{member.img}">
+                <div class="teamMemberText">
+                    <h1 class="teamMemberName">{member.name} - {member.title}</h1>
+                    <p>{member.bio}</p>
+                    {#if member.certifications}
+                        <h1 class="teamMemberName">Certifications</h1>
+                        <p>
+                            {#each member.certifications as certification, i}
+                                {#if i > 0}
+                                    &nbsp;-&nbsp;
+                                {/if}
+                                {certification}
+                            {/each}
+                        </p>
+                    {/if}
+                    <a class="greyButton" target="_blank" href="mailto:{member.email}?subject=Breakthrough%20Fit%20Co">Contact</a>
+                </div>
             </div>
-        </div>
-        <!-- Stephen Goodman-->
-        <div class="teamMember">
-            <img loading="lazy" alt="" src="Pictures/Staff/StephenGoodman.webp">
-            <div class="teamMemberText">
-                <h1 class="teamMemberName">Stephen Goodman - CFO &amp; COO</h1>
-                <p>Stephen is our CFO at Breakthrough Fit Co. He holds his Bachelor's Degree in Kinesiology from SUNY Cortland, as well as his MBA from Mercy College, graduating Summa Cum Laude. Stephen is a life-long resident of Westchester, and in his free time, enjoys exploring the beautiful nature the county has to offer, from hiking Bear Mountain, to the scenic Riverwalk in Tarrytown. Stephen is also a head coach for the Sound Shore Stars Special Olympics Team. His favorite food is Chicken Parmigiana! A quote he lives by is:<br><br>"Those who say they can and those that say they can't are both usually right."</p>
-                <a class="greyButton" target="_blank" href="mailto:steve@breakthroughfitco.com?subject=Breakthrough%20Fit%20Co">Contact</a>
-            </div>
-        </div>
-        <!-- Alexis Grier-->
-        <div class="teamMember">
-            <img loading="lazy" alt="" src="Pictures/Staff/AlexisGrier.webp">
-            <div class="teamMemberText">
-                <h1 class="teamMemberName">Alexis Grier - Coach</h1>
-                <p>Alexis has been working with the special needs community since 2012. She has a passion for working with children and families, fitness, and filmmaking. Her goal is to allow individuals to explore their physical and creative abilities, building self esteem and confidence in their ability to positively express themselves. She credits working as an Adaptive Special Needs Trainer and Behavioral Therapist as one of the most rewarding experiences in her career to date.</p>
-                <h1 class="teamMemberName">Certifications</h1>
-                <p>SPECIAL STRONG CERTIFIED</p>
-                <a class="greyButton" target="_blank" href="mailto:jake@breakthroughfitco.com?subject=Breakthrough%20Fit%20Co: Alexis Grier">Contact</a>
-            </div>
-        </div>
-        <!-- Vadid Bienaime-->
-        <div class="teamMember">
-            <img loading="lazy" alt="" src="Pictures/Staff/VadidBienaime.webp">
-            <div class="teamMemberText">
-                <h1 class="teamMemberName">Vadid Bienaime - Coach</h1>
-                <p>Vadid is from Haiti and has been working as a trainer since 2018. He has prior experience working with the special needs community in their group homes. He has a deep passion for fitness that he'd love to share with everyone, no matter their abilities. He enjoys running in his free time and his favorite food is pizza.</p>
-                <h1 class="teamMemberName">Certifications</h1>
-                <p>SPECIAL STRONG CERTIFIED</p>
-                <a class="greyButton" target="_blank" href="mailto:jake@breakthroughfitco.com?subject=Breakthrough%20Fit%20Co: Vadid Bienaime">Contact</a>
-            </div>
-        </div>
-        <!-- Taje Champagnie-->
-        <div class="teamMember">
-            <img loading="lazy" alt="" src="Pictures/Staff/TajeChampagnie.webp">
-            <div class="teamMemberText">
-                <h1 class="teamMemberName">Taje Champagnie - Coach</h1>
-                <p>As a professional with 6 years of training experience, Taje has come to the understanding that the greatest limiter is choice. He is a NASM certified Personal Trainer and specializes in balance and stability. Regardless of client goals, balance is the lowest common denominator, so his specialty is broad enough to facilitate a variety of physiological needs. He does this in order to remove as many obstacles as possible for his clients so that they get closer to that great limiter; choice. Once he helps his clients get there, they feel secure enough to make the decisions they need to regain, retain, or expand their health. A quote he lives by is:<br><br>"Those who ask for help -earnestly- deserve help."</p>
-                <a class="greyButton" target="_blank" href="mailto:taje@breakthroughfitco.com?subject=Breakthrough%20Fit%20Co">Contact</a>
-            </div>
-        </div>
-        <!-- Michelle Pachon-->
-        <div class="teamMember">
-            <img loading="lazy" alt="" src="Pictures/Staff/MichellePachon.webp">
-            <div class="teamMemberText">
-                <h1 class="teamMemberName">Michelle Pachon - Massage Therapist</h1>
-                <p>Meet Michelle Pachon, our NYS Licensed Massage Therapist and 200-hour Yoga Alliance Certified Instructor! Michelle has been a practicing Massage Therapist since 2015 and has extensive background in different restorative and regenerative massage styles such as Swedish, Myofascial release, Sport, Lymphatic, Prenatal, and Geriatric massage. She’s also known for her amazing Hatha, Power flow, Vinyasa flow and deep stretch yoga classes. Michelle is the best addition to your self-care routine!</p>
-                <a class="greyButton" target="_blank" href="mailto:michellesmassages14@gmail.com?subject=Breakthrough%20Fit%20Co">Contact</a>
-            </div>
-        </div>
+        {/each}
     </div>
-    </div>
-    <div class="padder">
-        <div class="centerBox">
-            <h2>Partners</h2>
-            <div id="partners" style="margin-bottom: 0;">
+</div>
+<div class="padder">
+    <div class="centerBox">
+        <h2>Partners</h2>
+        <div id="partners" style="margin-bottom: 0;">
             <div class="partner">
                 <div>
                 <img loading="lazy" alt="" src="Pictures/SpecialOlympicsNY.webp">
